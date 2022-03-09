74261149ddd42533cd00e163fd7a0c34f7271786
Indracit S <indrajithij802@gmail.com>
2022-03-09 18:28:36 +0530

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express=require('express')
 const app=express()
-const moment=require('moment')
 
 
 
-app.get('/api/:val',(req,res)=>{
-    let pattern=/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/gm
-    const {val}=req.params
-   if (pattern.test(val)){
-           const d = new Date(val);
-           let text = d.toUTCString();
-           console.log(d);
-           console.log(text);
-           var unixvalue=moment(text).unix()*1000
-           return res.status(200).json({unix:unixvalue})}
-    
-    else {
-        dateObj = new Date((val/1000)*1000);
-       d = dateObj.toUTCString();
-       console.log(d);
-        return res.status(200).json({"UTC":d})
-    }
+
+app.get('/api/:date',(req,res)=>{
+          
+    const {date}=req.params
+
+         if(!date){
+             var dat=new Date(); 
+         }
+         if(!isNaN(date)){
+             dat = new Date(parseInt(date)); 
+         }
+          else{
+             dat = new Date(date); 
+          }
+         
+         console.log(dat)
+  
+       if(dat.toString()=="Invalid Date"){
+       return res.json({"error":dat.toString()});
+   }    
+  res.json({unix:dat.valueOf(),"utc":dat.toUTCString()})
   })
   
   module.exports=app
